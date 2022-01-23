fe337a464d9f76e3883cc9597028632f20490e79
Edison Gu <hcgu_edison@outlook.com>
2022-01-23 10:05:03 -0500

    updated to compare the entire matrix

--- compare_data.py
+++ compare_data.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
 
-file_a = "./outputs/pickle.csv"
-file_b = "./outputs/T2000.csv"
+file_a = "./outputs/Chung/LTMG0.csv"
+file_b = "./outputs/Chung/T2000_LTMG.txt"
 
 table_a = pd.read_csv(file_a, index_col=0, sep=None).sort_index()
-table_b = pd.read_csv(file_b, index_col=0, sep=None).sort_index()
+table_b = pd.read_csv(file_b, index_col=0, sep=None).T.sort_index()
 
-print(table_a.head)
-print(table_b.head)
+# print(table_a.head)
+# print(table_b.head)
 
 idx_a = table_a.index.to_numpy()
 idx_b = table_b.index.to_numpy()
@@ -18,8 +18,11 @@ gene_diff = [g for g in idx_a if g not in idx_b]
 print(len(gene_diff))
 
 
-a = table_a.to_numpy()[:,:-1]
-b = table_b.to_numpy()[:,1:]
+a = table_a.to_numpy()
+b = table_b.to_numpy()
+
+print(a[:10,:10])
+print(b[:10,:10])
 
 diff = np.sum(np.abs(a-b))
 print(diff)
