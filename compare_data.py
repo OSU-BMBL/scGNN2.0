219c90a1ea3050ce310c76d9a5dbd9759c7a3f6b
Edison Gu <hcgu_edison@outlook.com>
2022-01-24 21:40:57 -0500

    Updated gitignore

--- compare_data.py
+++ compare_data.py
@@ -1,14 +1,22 @@
 import numpy as np
 import pandas as pd
 
-file_a = "./outputs/Chung/LTMG0.csv"
-file_b = "./outputs/Chung/T2000_LTMG.txt"
+data = '9'
 
-table_a = pd.read_csv(file_a, index_col=0, sep=None).sort_index()
-table_b = pd.read_csv(file_b, index_col=0, sep=None).T.sort_index()
+file_a = f"./outputs/{data}/T2000_expression.csv"
+file_b = f"./outputs/{data}/original_top_expression.csv"
 
-# print(table_a.head)
-# print(table_b.head)
+file_a = f"./outputs/{data}/T2000_LTMG.csv"
+file_b = f"./outputs/{data}/LTMG_0.0.csv"
+
+table_a = pd.read_csv(file_a, index_col=0, sep=None)#.sort_index()
+table_b = pd.read_csv(file_b, index_col=0, sep=None)#.sort_index()
+
+print(table_a.head(10))
+print(table_b.head(10))
+
+print(table_a.var(axis=1))
+print(table_b.var(axis=1))
 
 idx_a = table_a.index.to_numpy()
 idx_b = table_b.index.to_numpy()
@@ -17,12 +25,13 @@ gene_diff = [g for g in idx_a if g not in idx_b]
 
 print(len(gene_diff))
 
-
 a = table_a.to_numpy()
 b = table_b.to_numpy()
 
-print(a[:10,:10])
-print(b[:10,:10])
+# print(a[:10,:10])
+# print(b[:10,:10])
 
 diff = np.sum(np.abs(a-b))
 print(diff)
+
+print(a.sum()-b.sum())
