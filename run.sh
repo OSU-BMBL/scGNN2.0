03473557dd6f4f78dd426caaf8cd244a5286c4a8
Edison Gu <43914490+edisonghc@users.noreply.github.com>
2022-02-26 18:31:39 -0500

    Reverse run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=scGNN_v1.2.e.6.longer
 #SBATCH --time=5:00:00
-#SBATCH --output="%j_info_log.txt"
+#SBATCH --output="outputs/%j_info_log.txt"
 #SBATCH --account=PCON0022
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-dataset_name=Deng
-dropout_prob=0.1
-SLURM_JOB_ID=123
-
 set -e
 
 module load python/3.6-conda5.2
-source activate scgnnEnv
+source activate py-scgnn
 
-cd /fs/ess/scratch/PCON0022/ch/scGNN2.0/outputs
-if  [ -d  ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout ]; then
-rm  -rf  mkdir ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout
-fi
-
+cd /fs/ess/PCON0022/Edison/scGNN2.0/outputs
 mkdir ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout
-
 cd ..
 
-~/.conda/envs/ch/bin/python -W ignore /users/PCON0022/haocheng/scGNN/scGNN2.0-concat/scGNN_v2.py \
+python -W ignore scGNN_v2.py \
 --given_cell_type_labels \
 --load_use_benchmark \
---graph_AE_use_GAT \
 --load_dataset_dir /fs/ess/PCON0022/Edison/datasets \
 --load_dataset_name ${dataset_name} \
 --output_run_ID ${SLURM_JOB_ID} \
@@ -102,4 +92,3 @@ cd ..
 # sbatch --export=dataset_name=Treutlein,load_sc_dataset=Treutlein_expression.csv,load_cell_type_labels=Treutlein_cell_label.csv,dropout_prob=0 run.sh
 # sbatch --export=dataset_name=Usoskin,load_sc_dataset=Usoskin_expression.csv,load_cell_type_labels=Usoskin_cell_label.csv,dropout_prob=0 run.sh
 # sbatch --export=dataset_name=Yan,load_sc_dataset=Yan_expression.csv,load_cell_type_labels=Yan_cell_label.csv,dropout_prob=0 run.sh
-
