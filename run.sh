8f3d4f4f18580e9062c85344a5494a2e725b2057
Edison Gu <43914490+edisonghc@users.noreply.github.com>
2022-04-23 16:11:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=scGNN_vf.2.5
-#SBATCH --output="log/%j_info_log.txt"
-#SBATCH --time=100:00:00
+#SBATCH --output="outputs/%j_info_log.txt"
 #SBATCH --account=PCON0022
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-set -x
+set -e
 
-cd /fs/ess/scratch/PCON0022/ch/scGNN2.0/gat_outputs
-if  [ -d  ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout ]; then
-echo ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout
-echo "文件夹存在！"
-rm  -rf  mkdir ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout
-fi
+module load python/3.6-conda5.2
+source activate py-scgnn
 
+cd /fs/ess/PCON0022/edison/scGNN2.0/outputs
 mkdir ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout
+cd ..
 
-
-~/.conda/envs/ch/bin/python -W ignore /users/PCON0022/haocheng/scGNN/scGNN2.0/scGNN_v2.py \
+python -W ignore scGNN_v2.py \
 --given_cell_type_labels \
 --load_use_benchmark \
 --load_dataset_dir /fs/ess/PCON0022/edison/datasets \
 --load_dataset_name ${dataset_name} \
 --output_run_ID ${SLURM_JOB_ID} \
---output_dir ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout \
+--output_dir outputs/${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout \
 --dropout_prob ${dropout_prob} \
 --total_epoch 31 --feature_AE_epoch 500 300 \
 --output_intermediate \
