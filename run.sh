521ca20aab4729acf981a8620284c964a164e1e1
Haocheng Gu <gu649@pitzer-rw02.ten.osc.edu>
2022-04-28 16:17:47 -0400

    v1.3.0 final: less connection in graph

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=scGNN_vf.2.5
+#SBATCH --job-name=scGNN_v1.3.0
 #SBATCH --output="outputs/%j_info_log.txt"
 #SBATCH --account=PCON0022
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=1
+#SBATCH --mem=16G
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 set -e
@@ -15,7 +15,7 @@ cd /fs/ess/PCON0022/edison/scGNN2.0/outputs
 mkdir ${SLURM_JOB_ID}_${dataset_name}_${dropout_prob}_dropout
 cd ..
 
-python -W ignore scGNN_v2.py \
+python -W ignore scGNN_v2.py --use_bulk \
 --given_cell_type_labels \
 --load_use_benchmark \
 --load_dataset_dir /fs/ess/PCON0022/edison/datasets \
@@ -25,8 +25,8 @@ python -W ignore scGNN_v2.py \
 --dropout_prob ${dropout_prob} \
 --total_epoch 31 --feature_AE_epoch 500 300 \
 --output_intermediate \
---graph_AE_neighborhood_factor 0.1 --graph_AE_graph_construction v2 \
+--graph_AE_neighborhood_factor 0.05 --graph_AE_graph_construction v2 \
 --graph_AE_normalize_embed sum1 --graph_AE_use_GAT
 
-# --graph_AE_retain_weights
+# --graph_AE_retain_weights #SBATCH --gpus-per-node=1
 # --graph_AE_concat_prev_embed --graph_AE_normalize_embed sum1 binary # --clustering_use_flexible_k --clustering_embed both 
